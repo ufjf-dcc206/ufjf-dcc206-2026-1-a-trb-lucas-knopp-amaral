cf395c31247cdd80ef6bc8ec67777aaf19745616
Alusc <lucasknoppamaral@protonmail.com>
2026-06-29 16:31:18 -0300

    Adiciona propriedades privadas da clase Minesweeper

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,4 +1,16 @@
 class Minesweeper extends HTMLElement {
+    #cellSize = 40;
+    #width = 9;
+    #height = 9;
+    #bombs = 15;
+    #cells = [];
+    #isFirstClick = true;
+    #hoveringCell = null;
+    #consumeLeftClick = false;
+    #markedSquares = 0;
+    #safeSquares = 0;
+    #holdTimeoutId = null;
+    #timeToHold = 200;
 
     constructor() {
         super();
